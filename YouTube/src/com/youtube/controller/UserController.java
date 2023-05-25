ba991e8a3e97fd79c82ee971e09028a142acc7e0
wndud6328 <wndud6328@naver.com>
2023-05-25 15:03:03 +0900

    0525

---
+++ YouTube/src/com/youtube/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.youtube.controller;
+
+public class UserController {
+
+	public boolean login() {// 로그인
+		return false;
+	}
+	
+	public boolean signUp() { // 회원가입
+		return false;
+	}
+
+	public UserController viewProfile() { // 프로필 보기
+		return null;
+	}
+	
+	public UserController updateProfile() { // 프로필 수정
+		return null;
+	}
+	
+	public boolean deleteProfile() { // 계정 삭제
+		return false;
+	}
+}
+
---
+++ YouTube/src/com/youtube/controller/VideoController.java
@@ -0,0 +1,42 @@
+package com.youtube.controller;
+
+import java.util.Date;
+
+import com.youtube.model.Video;
+
+public class VideoController {
+
+	private VideoController v = new VideoController();
+	
+	public VideoController() {}
+	
+	/*
+	 * Create : 추가
+	 * Read : 읽기 (1개, 목록)
+	 * Update : 수정
+	 * Delete : 삭제
+	 * */
+	
+	public boolean saveData(VideoController v) {
+		
+		this.v.setTitle(v.getTitle());
+		this.v.setcomment(v.getcommet());
+		this.v.setUploadAt(v.getUploadAt());
+		this.v.setCount(v.getCount());
+		this.v.setimgUrl(v.getImgUrl());
+		this.v.setFileUrl(v.getFileurl());
+	
+		return true;
+	}
+	public VideoController confirmData() {
+		return this.v;
+	}
+	
+
+
+}
+
+
+
+
+
